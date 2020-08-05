8181a202cf30de462d9f8dfe5f99bc34280cd475
Akhil-Atyam <68386984+Akhil-Atyam@users.noreply.github.com>
2020-08-05 16:23:10 -0400

    updated sketch file for project 23
    updated sketch file for project 23
    makes code simpler

--- sketch.js
+++ sketch.js
@@ -1,57 +1,77 @@
-
-
-
-
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG,a1,a2,a3,a;
 var packageBody,ground
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-engine = Engine.create();
-world = engine.world;
 
 function preload()
 {
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
+    helicopterIMG=loadImage("helicopter.png")
+    packageIMG=loadImage("package.png")
 }
 
 function setup() {
-	createCanvas(800, 600);
-	rectMode(CENTER);
-	
+    createCanvas(800, 700);
+    rectMode(CENTER);
+        
+    packageSprite=createSprite(width/2, 80, 10,10);
+    packageSprite.addImage(packageIMG)
+    packageSprite.scale=0.2
 
-	a1 = new Zone(width/2,height-45,200,10);
-	a2 = new Zone(300,height-90,10,100);
-	a3 = new Zone(500,height-90,10,100);
+    helicopterSprite=createSprite(width/2, 200, 10,10);
+    helicopterSprite.addImage(helicopterIMG)
+    helicopterSprite.scale=0.6
+
+    groundSprite=createSprite(width/2, height-35, width,10);
+    groundSprite.shapeColor=color(255)
+
+    engine = Engine.create();
+    world = engine.world;
+
     
-	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
+    packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+    World.add(world, packageBody);
+    
 
-	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
+    //Create a Ground
+    ground = Bodies.rectangle(width/2, 550, width, 20 , {isStatic:true} );
+    World.add(world, ground);
 
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
+    /*
+     a1 = new Zone(width/2-100,height-50,200,20);
 
+     a2 = new Zone(300,height-90,20,100);
+     
+     a3 = new Zone(500,height-90,20,100);
+    */
 
-	
-	packageBody = Bodies.circle(width/2 , 200 , 23, {restitution:0.6, isStatic:true});
-	World.add(world, packageBody);
-	
+    
+    boxPosition=width/2-100
+    boxY=530;
 
-	//Create a Ground
-	//ground = Bodies.rectangle(width/2, 550, width, 20 , {isStatic:true} );
- 	//World.add(world, ground);
+    boxleftSprite=createSprite(boxPosition, boxY, 20,100);
+    boxleftSprite.shapeColor=color(255,0,0);
 
+    boxLeftBody = Bodies.rectangle(boxPosition, boxY, 20,100 , {isStatic:true} );
+    World.add(world, boxLeftBody);
 
-	Engine.run(engine);
+    boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+    boxBase.shapeColor=color(255,0,0);
+
+    boxBottomBody = Bodies.rectangle(boxPosition-100, boxY+45, 200,20 , {isStatic:true} );
+    World.add(world, boxBottomBody);
+
+    boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
+    boxleftSprite.shapeColor=color(255,0,0);
+
+    boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+    World.add(world, boxRightBody);
+    
+
+    Engine.run(engine);
   
 }
-
 
 function draw() {
   rectMode(CENTER);
@@ -59,9 +79,11 @@ function draw() {
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
   drawSprites();
+  /*
   a1.display();
   a2.display();
   a3.display();
+  */
 }
 
 function keyPressed() {
@@ -70,6 +92,7 @@ function keyPressed() {
     Body.setStatic(packageBody,false);
   }
 }
+
 
 
 
